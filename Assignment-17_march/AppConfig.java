e4b644bd3c2628306305eb5cbb13affc1932f7b4
AyushSirohi <sirohiayush977@gmail.com>
2026-03-18 14:35:13 +0530

    Add AppConfig class for Spring configuration

---
+++ Assignment-17_march/AppConfig.java
@@ -0,0 +1,9 @@
+package assignment-17march;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "assignment-17march")
+public class AppConfig {
+}
