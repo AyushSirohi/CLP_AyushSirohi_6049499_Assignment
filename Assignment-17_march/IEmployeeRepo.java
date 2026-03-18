273146cdf19705115f7336bdbeea83169d4cfcfe
AyushSirohi <sirohiayush977@gmail.com>
2026-03-18 14:33:23 +0530

    Create IEmployeeRepo interface for employee management

---
+++ Assignment-17_march/IEmployeeRepo.java
@@ -0,0 +1,15 @@
+package assignment-17march;
+
+import java.util.*;
+
+public interface IEmployeeRepo {
+	void insert(Employee emp);
+
+    List<Employee> getAll();
+
+    Employee getById(int id);
+
+    boolean update(Employee emp);
+
+    boolean delete(int id);
+}
